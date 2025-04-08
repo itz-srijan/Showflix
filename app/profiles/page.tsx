548d0c508e002c7bd9f357fd78ecbb88664553f0
itz-srijan <122741991+itz-srijan@users.noreply.github.com>
2025-04-08 23:16:22 +0530

    button added

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -3,15 +3,17 @@
 import { useRouter } from "next/navigation";
 
 const Profiles = () => {
-    const router = useRouter();
-    return (
-        <div>
-            <p className="text-black text-4xl">Profiles</p>
+  const router = useRouter();
+  return (
+    <div>
+      <p className='text-black text-4xl'>Profiles</p>
 
-            <div>
-                <div onClick={() => router.push('/')}>who is watching</div>
-            </div>
+      <div>
+        <div onClick={() => router.push("/")}>
+          <button>click here</button>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 export default Profiles;
